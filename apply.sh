1b664c81fe9964dcb3fa33b1a3d02705622507c8
mjtech009 <113666954+mjtech009@users.noreply.github.com>
2023-08-03 05:18:01 +0530

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -4,6 +4,6 @@ timestamp=$(date +"%Y-%m-%d-%H-%M-%S")
 log_file="apply_logs_$timestamp.txt"
 exec &> "$log_file"
 curl \
-  --header "Authorization: Bearer $TOKEN" \
+  --header "Authorization: Bearer $TF_API_TOKEN" \
   --header "Content-Type: application/vnd.api+json" \
   https://app.terraform.io/api/v2/applies/apply-47MBvjwzBG8YKc2v
